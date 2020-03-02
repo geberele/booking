5e18344a602e16280fc87199eb804d3bd0df279d
Gab <geberele@gmail.com>
2020-03-02 22:29:15 +0000

    Adding the build folder

---
+++ build/precache-manifest.139fdbb9b82f1b03db10f94bf90b7d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820b4daae04185ef2a85fa4b446413f6",
+    "url": "/booking/index.html"
+  },
+  {
+    "revision": "d0593d78dbc22bd57475",
+    "url": "/booking/static/css/main.f6f8974a.chunk.css"
+  },
+  {
+    "revision": "790d07aefcd2bb211f6a",
+    "url": "/booking/static/js/2.0b57a2f9.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/booking/static/js/2.0b57a2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0593d78dbc22bd57475",
+    "url": "/booking/static/js/main.ec9fe172.chunk.js"
+  },
+  {
+    "revision": "c2826b26d825d4060100",
+    "url": "/booking/static/js/runtime-main.e1eb28f8.js"
+  }
+]);
